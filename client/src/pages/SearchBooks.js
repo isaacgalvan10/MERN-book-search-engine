963a1a215d4bb17697e5bcbb7966a1fb5660d211
Isaac Galvan <isaacgdev@gmail.com>
2022-04-28 15:13:21 -0500

    updating files to use mutations, and queries

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,10 +8,11 @@ import {
   Card,
   CardColumns,
 } from 'react-bootstrap';
-import { useMutation, useQuery } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
+import { useMutation } from '@apollo/client';
+
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -61,6 +62,8 @@ const SearchBooks = () => {
     }
   };
 
+  const [saveBook, { error, data }] = useMutation(SAVE_BOOK);
+
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -74,9 +77,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { ...bookToSave },
+      });
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
